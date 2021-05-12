a0ff13bd4417501d4e28f420280c0ad23b045885
lucas.matos <lucas.ssouza.mmatos@gmail.com>
2021-05-12 18:02:13 -0300

    refactor visualization add the type from data graphic

--- src/onsklearn/visualization.py
+++ src/onsklearn/visualization.py
@@ -2,13 +2,27 @@ import matplotlib.pyplot as plt
 from normalizer import Normalizer 
 
 class Visualization:
-  def __init__(self) -> None:
+  virginica = 'virginica'
+  versicolor = 'versicolor'
+  setosa = 'setosa'
+
+  def __init__(self, dataset_types = [virginica, versicolor, setosa]) -> None:
+    self.dataset_types = dataset_types
     self.normalizer = Normalizer()
 
+  def __configure_graphic_data__(self, x):
+    if self.setosa in self.dataset_types:
+      plt.scatter(x[:50, 0], x[:50, 1], color='red', marker='o', label='setosa')
+
+    if self.versicolor in self.dataset_types:
+      plt.scatter(x[50:100, 0], x[50:100, 1], color='blue', marker='o', label='versicolor')
+    
+    if self.virginica in self.dataset_types:
+      plt.scatter(x[100:150, 0], x[100:150, 1], color='green', marker='o', label='virginica')
+
   def __visualize__(self, x, xlabel, ylabel):
-    plt.scatter(x[:50, 0], x[:50, 1], color='red', marker='o', label='setosa')
-    plt.scatter(x[50:100, 0], x[50:100, 1], color='blue', marker='o', label='versicolor')
-    plt.scatter(x[100:150, 0], x[100:150, 1], color='green', marker='o', label='virginica')
+    self.__configure_graphic_data__(x)
+    
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.legend(loc='upper left')
