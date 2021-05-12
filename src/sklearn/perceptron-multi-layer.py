d4288d5a1bdb5b1c892c6f26d722f3e6c9889184
lucas.matos <lucas.ssouza.mmatos@gmail.com>
2021-05-12 13:02:49 -0300

    refactor perceptron multilayer

--- src/sklearn/perceptron-multi-layer.py
+++ src/sklearn/perceptron-multi-layer.py
@@ -1,57 +1,73 @@
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 
+class PerceptronMultiLayer:
+    csv_data = 'dataset/iris.csv'  # file from work data
 
-dataset = pd.read_csv('dataset/iris.csv')
-index_names = dataset[dataset['Species'] == 'Iris-setosa'].index
-dataset.drop(index_names, inplace=True)
-dataset.loc[dataset['Species'] == 'Iris-versicolor', 'Species'] = 0
-dataset.loc[dataset['Species'] == 'Iris-virginica', 'Species'] = 1
-dataset.drop('Id', inplace=True, axis=1)
+    def __init__(self) -> None:
+      self.__setup__()
 
-dataset = dataset.reset_index()
-dataset = dataset.apply(pd.to_numeric)
+    def __setup__(self) -> None:
+      self.mlp = MLPClassifier(
+        hidden_layer_sizes=10, 
+        learning_rate_init=0.01, 
+        max_iter=500, 
+        random_state=105
+      )
+      self.dataset = pd.read_csv(self.csv_data)
+      self.__normalize_data__()
+      self.__separate_species__()    
+      self.__set_data_train__()   
 
-dataset_versicolor = dataset.loc[dataset.index < 50]
-dataset_virginica = dataset.loc[dataset.index >= 50]
+    '''
+      @ remove the iris setosa from datase
+      @ specifies the species as number
+      @ reset the dataset index after droppes and convert all info to number
+    '''
+    def __normalize_data__(self) -> None:
+      index_names = self.dataset[self.dataset['Species'] == 'Iris-setosa'].index
+      self.dataset.drop(index_names, inplace=True)
 
-dataset_versicolor = dataset_versicolor.reset_index()
-dataset_virginica = dataset_virginica.reset_index()
+      self.dataset.loc[self.dataset['Species'] == 'Iris-versicolor', 'Species'] = 0
+      self.dataset.loc[self.dataset['Species'] == 'Iris-virginica', 'Species'] = 1
+      self.dataset.drop('Id', inplace=True, axis=1)
 
-dataset_train_versicolor = dataset_versicolor.loc[dataset_versicolor.index < 35]
-dataset_train_virginica = dataset_virginica.loc[dataset_virginica.index < 35]
+      self.dataset = self.dataset.reset_index()
+      self.dataset = self.dataset.apply(pd.to_numeric)
 
-dataset_test_versicolor = dataset_versicolor.loc[dataset_versicolor.index >= 35]
-dataset_test_virginica = dataset_virginica.loc[dataset_virginica.index >= 35]
+    '''
+      separates the dataset where clause for specie is setted
+    '''
+    def __separate_specie__(self, where):
+      return self.dataset.loc[where]
 
-dataset_train = pd.concat([dataset_train_versicolor, dataset_train_virginica])
-dataset_test = pd.concat([dataset_test_versicolor, dataset_test_virginica])
+    '''
+      separates the dataset between species
+    '''
+    def __separate_species__(self):
+      self.dataset_versicolor = self.__separate_specie__(self.dataset.index < 50)
+      self.dataset_virginica = self.__separate_specie__(self.dataset.index >= 50)
+      self.dataset_versicolor = self.dataset_versicolor.reset_index()
+      self.dataset_virginica = self.dataset_virginica.reset_index()
 
-# training_set = dataset.values
-#
-X_train = dataset_train.loc[:, ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
-y_train = dataset_train['Species']
+    '''
+      set the first 35 of dataset to data for the network train
+    '''
+    def __set_data_train__(self):
+      dataset_train_versicolor = self.dataset_versicolor.loc[self.dataset_versicolor.index < 35]
+      dataset_train_virginica = self.dataset_virginica.loc[self.dataset_virginica.index < 35]
+      self.dataset_train = pd.concat([dataset_train_versicolor, dataset_train_virginica])
 
-X_test = dataset_test.loc[:, ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
-y_test = dataset_test['Species']
+    ''' 
+      train the model
+    '''
+    def __train__(self):
+      x_train = self.dataset_train.loc[:, ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+      print(x_train)
+      
+      y_train = self.dataset_train['Species']
+      print(y_train)
+      self.mlp.fit(x_train, y_train)
 
-# # X_train, X_test, y_train, y_test = train_test_split(training_set[:, :4],
-# #                                                     training_set[:, 4],
-# #                                                     test_size=0.2)
-
-mlp = MLPClassifier(hidden_layer_sizes=10,
-                    learning_rate_init=0.01,
-                    max_iter=500,
-                    random_state=105)
-
-
-# Train the model
-mlp.fit(X_train, y_train)
-# Test the model
-# print(mlp.score(X_test, y_test))
-# print(X_test)
-print(X_test.values[14])
-print(y_test.values[14])
-print(mlp.predict([X_test.values[14]]))
-
-# print()
+    def execute(self) -> None:
+      return self.__train__()
